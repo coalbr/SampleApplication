1ee958b68556cbddaf2cb6c57ac0a79580932cd6
Austin Albrecht <austin@pyraego.com>
2017-11-13 17:57:56 -0500

    Added category for colors.

---
+++ SampleApplication/Helpers/Categories/UIColor+CustomColors.h
@@ -0,0 +1,16 @@
+//
+//  UIColor+CustomColors.h
+//  SampleApplication
+//
+//  Created by Austin Albrecht on 11/13/17.
+//  Copyright © 2017 Austin Albrecht. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (CustomColors)
+
++ (UIColor *)mediumBlueColor;
++ (UIColor *)mediumGreenColor;
+
+@end
---
+++ SampleApplication/Helpers/Categories/UIColor+CustomColors.m
@@ -0,0 +1,24 @@
+//
+//  UIColor+CustomColors.m
+//  SampleApplication
+//
+//  Created by Austin Albrecht on 11/13/17.
+//  Copyright © 2017 Austin Albrecht. All rights reserved.
+//
+
+#import "UIColor+CustomColors.h"
+
+@implementation UIColor (CustomColors)
+
+#pragma mark - Public methods
++ (UIColor *)mediumBlueColor
+{
+    return [UIColor colorWithRed:45/255.0 green:106/255.0 blue:171/255.0 alpha:1.0];
+}
+
++ (UIColor *)mediumGreenColor
+{
+    return [UIColor colorWithRed:29/255.0 green:107/255.0 blue:38/255.0 alpha:1.0];
+}
+
+@end
